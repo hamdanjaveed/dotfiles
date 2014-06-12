869bdeab58642d6c5616567edc5e1d0d10badc6c
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-06-12 15:48:03 -0400

    Formatted print statements

--- install.sh
+++ install.sh
@@ -12,11 +12,27 @@ for file in $oldFilePath; do
     rm -f file
 done
 
+# get the length of the longest file name
+longestName=0
+
+for file in $HOME/.*; do
+    name=${file#${hiddenFilePath}}
+    if [ -f $rootpath$name ]; then
+	tempSize=${#name}
+	if [ $((tempSize)) -gt $((longestName)) ]; then
+	    longestName=$tempSize
+        fi
+    fi
+done
+
 # Backing up
 for file in $HOME/.*; do
     name=${file#${hiddenFilePath}}
     if [ -f $rootpath$name ]; then
-        printf "Backing up $name..."
+	printf "Backing up $name"
+	newSize=${#name}
+	finalSize=$((newSize - longestName - 3))
+	printf '.%.0s' $(seq 1 $finalSize)
         mv $HOME/.$name $oldFilePath/$name
         printf "Done\n"
     fi
@@ -27,9 +43,15 @@ printf "Files backed up\n\n"
 # Link new files
 for file in $rootpath*; do
     name=${file#${rootpath}}
-    printf "Linking $name..."
+    printf "Linking up $name"
+    newSize=${#name}
+    finalSize=$((newSize - longestName - 3))
+    printf '.%.0s' $(seq 1 $finalSize)
+
     ln -s "$HOME/dotfiles/files/$name" $HOME/.$name
     printf "Done\n"
 done
 
-printf "\n... Finished\n"
+printf "Files linked\n\n"
+
+printf "... Finished"
