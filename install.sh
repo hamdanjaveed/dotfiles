9bf897996ef0536a21ff3fd5d9f2a5af7c826ef9
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-07-15 16:04:19 -0400

    Install.sh: skip asking for root permission if not neccessary

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 
 printf "Installing dotfiles...\n"
 
-restartRequired=false
+installOSXPrefs=false
 
-# Ask for root permissions
-sh scripts/askroot.sh
+if [ $# -ne 0 ] && [ "$1" = "-a" ];
+then
+    installOSXPrefs=true
+fi
+
+if [ "$installOSXPrefs"=true ];
+then
+    # Ask for root permissions
+    sh scripts/askroot.sh
+fi
 
 # Install dotfiles
 sh scripts/dotfiles.sh
@@ -13,18 +21,17 @@ sh scripts/dotfiles.sh
 # Set up git preferences
 sh scripts/gitprefs.sh
 
-if [ $# -ne 0 ] && [ "$1" = "-a" ];
+if [ "$installOSXPrefs" = true ];
 then
     # Install OS X preferences
     sh scripts/osxprefs.sh
-    restartRequired=true
 else
     printf "Skipping OS X preferences (to set preferences, use the -a flag)\n"
 fi
 
 printf "\n... Finished"
 
-if [ "$restartRequired" = true ]
+if [ "$installOSXPrefs" = true ];
 then
     printf ", a restart may be required"
 fi
