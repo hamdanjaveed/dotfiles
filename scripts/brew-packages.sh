e76420bb0f9262b2461b28b442b2af7994250679
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-09-28 01:31:10 -0400

    Brew Packages: +valgrind

--- scripts/brew-packages.sh
+++ scripts/brew-packages.sh
@@ -5,7 +5,7 @@ printf "Checking for missing brew packages...\n"
 # Check and install brew packages
 brewList=$(brew list)
 
-declare -A brewPackages=(["git"]="git" ["cmake"]="cmake" ["unrar"]="unrar" ["glfw3"]="homebrew/versions/glfw3" ["glew"]="glew" ["glm"]="glm" ["libpng"]="libpng" ["brew-cask"]="caskroom/cask/brew-cask" ["node"]="node" ["phantomjs"]="phantomjs" ["casperjs"]="casperjs" ["wget"]="wget" ["bash"]="bash" ["tmux"]="tmux" ["sdl2"]="sdl2" ["unrar"]="unrar")
+declare -A brewPackages=(["git"]="git" ["cmake"]="cmake" ["unrar"]="unrar" ["glfw3"]="homebrew/versions/glfw3" ["glew"]="glew" ["glm"]="glm" ["libpng"]="libpng" ["brew-cask"]="caskroom/cask/brew-cask" ["node"]="node" ["phantomjs"]="phantomjs" ["casperjs"]="casperjs" ["wget"]="wget" ["bash"]="bash" ["tmux"]="tmux" ["sdl2"]="sdl2" ["unrar"]="unrar" ["valgrind"]="valgrind")
 
 for package in "${!brewPackages[@]}"; do
     if [[ $brewList != *$package* ]]; then
